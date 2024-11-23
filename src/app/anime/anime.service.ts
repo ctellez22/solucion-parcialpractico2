808f3c361e697bbfc1919dbe90683cfe83f29d30
ctellez22 <142060772+ctellez22@users.noreply.github.com>
2024-11-23 10:33:21 -0500

    Add files via upload

---
+++ src/app/anime/anime.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+import { Observable, map } from 'rxjs';
+import { Anime } from './anime';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimeService {
+
+  private apiUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAnimes(): Observable<Anime[]> {
+    return this.http.get<Anime[]>(this.apiUrl);
+  }
+
+  getAnime(id: string): Observable<Anime> {
+    return this.http.get<Anime[]>(this.apiUrl).pipe(
+      map((animes: Anime[]) => {
+        //Complete con el código necesario para recorrer los animes y retornar el anime con el id buscado
+        throw new Error(`Anime con ID ${id} no encontrado`);
+      })
+    );
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+const baseUrl =
+ 'https://gist.githubusercontent.com/VivianGomez/cc4116c67063bed35e9a39723e7a9d6a/raw/7699873b61ee4c4fde7da9f9ee8c4a125cbdac05/anime.json';
+
+export const environment = {
+ production: false,
+ baseUrl
+};
+
